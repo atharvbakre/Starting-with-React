67637bda8de16af932db62b0482c5bdf2a03ecbb
Atharv Bakre <44296079+atharvbakre@users.noreply.github.com>
2021-05-17 20:06:29 +0530

    Used Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,47 @@
-import { Component } from 'react'
+import { useState } from 'react'
 import Header from './Header'
 import TodoItem from './TodoItem'
 import todoData from '../todoData'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      todoItems: todoData
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleAddTask = this.handleAddTask.bind(this)
-    this.count = 1
-  }
+function App() {
 
-  handleChange(id) {
-    const updatedTodoList = this.state.todoItems.map(task => {
-      if(task.id === id) task.completed = !task.completed
-      return task
+  const [state, setState] = useState({ todoItems: todoData, count: 1 });
+  
+  const handleChange = (id) => {
+    const updatedTodoList = state.todoItems.map(task => {
+      if(task.id === id) task.completed = !task.completed;
+      return task;
     })
-
-    this.setState({ todoItems: updatedTodoList })
+    
+    setState({ todoItems: updatedTodoList });
   }
-
-  handleAddTask() {
-    if(!document.getElementById('task').value) return
-
+  
+  const handleAddTask = () => {
+    if(!document.getElementById('task').value) return;
+    
     todoData.push({
-      id: this.count,
+      id: state.count,
       description: document.getElementById('task').value,
       completed: false
     })
+    setState({ todoItems: todoData, count: state.count+1 });
     
-    this.setState({ todoItems: todoData })
-
     document.getElementById('task').value = null
     document.getElementById('task').focus()
-    this.count++;
   }
   
-  render() {
-    const taskList = this.state.todoItems.map(task => <TodoItem key={task.id} task={task} handleChange={this.handleChange} />)
-    return (
-      <div>
-        <Header />
-        <div className="add-task">
-            <input id="task" type="text" placeholder="Task"/>
-            <button type='submit' id="add-task-button" onClick={this.handleAddTask}>Add Task</button>
-        </div>
-        <div className="todo-list">
-          {taskList}
-        </div>
+  return (
+    <div>
+      <Header />
+      <div className="add-task">
+          <input id="task" type="text" placeholder="Task"/>
+          <button type='submit' id="add-task-button" onClick={(e) => handleAddTask(e)}>Add Task</button>
       </div>
-    );
-  }
+      <div className="todo-list">
+        {state.todoItems.map(task => <TodoItem key={task.id} task={task} handleChange={handleChange} />)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
